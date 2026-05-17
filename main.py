eea07a51942ac83912f71687c4b237a02e48fab4
ikitron <130728002+ikitron@users.noreply.github.com>
2026-05-16 18:29:24 -0600

    Update main.py

--- main.py
+++ main.py
@@ -23,7 +23,9 @@ intents = discord.Intents.default()
 intents.message_content = True
 
 bot = commands.Bot(command_prefix="!", intents=intents, help_command=None)
-
+@app.get("/")
+def index():
+    return "", 200
 # =========================
 # DATABASE
 # =========================
